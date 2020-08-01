22192f701450fe5ef7dac63746cb8f78eff31201
Michael Schwartz <mykesx@gmail.com>
2020-07-31 17:34:54 -0700

    posts messages to slack for comments less than 1 hour old

--- debug.sh
+++ debug.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-docker-compose -f docker-compose-dev.yml up
+# if you want to drop the database, you can use this command:
+#   ./debug.sh DROP
+
+DROP=$1 docker-compose -f docker-compose-dev.yml up
--- index.js
+++ index.js
@@ -2,99 +2,42 @@
  * youtube-slack-bot
  */
 
-process.env.DEBUG='MongoDB';
+process.env.DEBUG='topLevelComments newComments';
+
 
 
 const debug = require("debug")("youtube-slack-bot"),
-      Slack = require("./lib/Slack"),
-      slack = new Slack(process.env.MODUS_SLACK_SECRET),
-      YouTube = require("./lib/YouTube"),
-      yt = new YouTube(),
+      slack = require("./lib/Slack"),
+      yt = require("./lib/YouTube"),
       MongoDB = require("./lib/MongoDB");
 
-const topLevelComments = async () => {
-  try {
-    const videos = await yt.queryChannel();
-    console.log("Number of videos:", videos.count);
-    const comments = await yt.queryComments();
-
-    for (let item of comments) {
-      const snippet = item.snippet,
-	    key = item.snippet.videoId,
-	    comment = item.snippet,
-	    video = videos[key];
-
-      video.comments = video.comments || [];
-      video.comments.push(comment);
-    }
-
-    const sorted = [];
-    for (let key of Object.keys(videos)) {
-      if (key == 'count') {
-	continue;
-      }
-      const video = videos[key],
-	    snippet = video.snippet;
-      video.comments = video.comments || [];
-      video.comments = video.comments.sort((c1, c2) => {
-	const snip1= c1.topLevelComment.snippet,
-	      snip2 = c2.topLevelComment.snippet;
-	const d1 = new Date(snip1.publishedAt),
-	      d2 = new Date(snip2.publishedAt);
-	return d2 - d1;
-      });
-      
-      const now = new Date();
-      for (let comment of video.comments) {
-	const csnippet = comment.topLevelComment.snippet,
-	      replyCount = comment.totalReplyCount,
-	      d1 = new Date(csnippet.publishedAt),
-	      elapsed = Math.round((now - d1)  / (1000 * 60 * 60 * 24));
-	if (elapsed >= 2 && replyCount == 0) {
-	  sorted.push({
-	    replyCount: replyCount,
-	    video: csnippet,
-	    elapsed: elapsed,
-	    date: d1,
-	    text: `Comment on video ${snippet.title} has not been replied to for ${elapsed}  days.`});
-	}
-      }
-    }
-
-    sorted.sort((a, b) => {
-      return a.date - b.date;
-    });
-
-    for (let comment of sorted) {
-      await slack.SendMessage('youtube-slack-bot', comment.text);
-      
-    }
-  }
-  catch (e) {
-    console.error("error", e);
-  }
-};
+const topLevelComments = require('./functions/topLevelComments'),
+      newComments = require('./functions/newComments');
 
 const wait = ms => new Promise(res => setTimeout(res, ms));
+
+const HOUR = 60 * 60 * 1000,
+      DAY = 24 * HOUR;
 
 const main = async () => {
   await slack.SendMessage('youtube-slack-bot', "  ");
   await slack.SendMessage('youtube-slack-bot', "  ");
-  await slack.SendMessage('youtube-slack-bot', "youtube-slack-bot started");
-  await slack.SendMessage('youtube-slack-bot', "  ");
-  await slack.SendMessage('youtube-slack-bot', "  ");
-  const DAY = 24 * 60 * 60 * 1000,
-	WAIT_TIME = DAY ;
+  await slack.SendMessage('youtube-slack-bot', `youtube-slack-bot started (${process.env.NODE_ENV})`);
 
   for (;;) {
-    topLevelComments();
-    await wait(WAIT_TIME);
+    for (let hour = 0; hour<24; hour++) {
+      const videos = await yt.queryChannel(),
+	    comments = await yt.queryComments();
+      await topLevelComments(videos, comments);
+      await newComments(videos);
+      await wait(HOUR);
+    }
   }
 };
 
 const drop = async () => {
   try {
-    const result = MongoDB.DropDatabase();
+    const result = await MongoDB.DropDatabase();
     console.log(result);
   }
   catch (e) {
@@ -115,6 +58,13 @@ const foo = async() => {
   }
 };
 
-// drop();
-main();
+console.log("DROP", process.env.DROP);
+if (process.env.DROP) {
+  console.log("Dropping database");
+  drop();
+  process.exit(1);
+}
+else {
+  main();
+}
 
